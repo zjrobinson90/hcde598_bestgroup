1200ffa23b4b0b6bfaac245fb40a261243793a17
Zack <zjrobinson90@gmail.com>
2017-03-06 21:00:38 -0800

    added car and some quality check stuff

--- carEthics/sketch.js
+++ carEthics/sketch.js
@@ -1,28 +1,95 @@
-var arrayX = ["A", "B", "C", "D", "E", "F", "G", "H"];
+// Created by Josh Leibsohn and Zechariah Robinson
+// Last edited 03/06/17
+// This is some code to crowd-source ethics for self-driving cars
+
+var arrayX = ["One kid", "Group of kids", "One adult", "Group of adults", "Paul Rudd", "Jeff Bezos", "Donald Trump", "The Pope"];
 var arrayY = [];
-var used1 = [];
-var used2 = [];
 var value = [];
+var results = [];
 var randX = 0;
 var randY = 0;
+var speed = 1 * 7;
+var dir = 0;
+var carX = 100;
+var carY = 250;
+var on = 0;
+var totalCombinations = arrayX.length - 1;
 
 function setup() {
   createCanvas(800, 600);
   for (i = 0; i < arrayX.length; i++) {
     append(arrayY, arrayX[i]);
+    append(results, "choice" + i);
+    results[i] = 0;
   }
+  findCombinations();
 }
 
 function draw() {
-  background(200);
+  background(100, 200, 0);
   qualityCheck();
   road();
+  car(carX, carY);
   showOptions();
+  turnCarOn();
+  if(value.length == totalCombinations){
+    background(255);
+    fill(0);
+    resultsLocal();
+    text(results[3], 100, 100);
+  }
+}
+
+
+function resultsLocal() {
+  for(i = 0; i < arrayX.length; i++) {
+    for(j = 0; j < value.length; j++) {
+      if(value[j].winner == arrayX[i]) {
+        results[i] = results[i] + 1;
+      }
+    }
+  }
+}
+
+function findCombinations() {
+  for(i = 2; i < arrayX.length; i++) {
+    totalCombinations = totalCombinations + arrayX.length - i;
+  }
+}
+
+function turnCarOn() {
+  if(mouseIsPressed) {
+    on = 1;
+  }
+  if(on == 1) {
+    carAnimation();
+  }
+  if(carX == width - 200) {
+    on = 0;
+  }
 }
 
 function road() {
   fill(50);
   rect(0, height / 3, width, height / 3);
+}
+
+function car(x, y) {
+  fill(255, 0, 0);
+  rect(x, y, 100, 50);
+  rect(x + 25, y - 25, 50, 25);
+  fill(0);
+  ellipse(x + 25, y + 50, 30);
+  ellipse(x + 75, y + 50, 30);
+}
+
+function carAnimation() {
+  carX = carX + speed;
+  carY = carY + dir;
+  if(carX >= width - 200) {
+    carX = width - 200;
+    carY = carY - dir;
+  }
 }
 
 function showOptions() {
@@ -39,9 +106,6 @@ function mouseReleased() {
   assignValue();
   randX = int(random(arrayX.length));
   randY = int(random(arrayY.length));
-  append(used1, arrayX[randX]);
-  append(used2, arrayY[randY]);
-  print(value);
 }
 
 function qualityCheck() {
@@ -49,15 +113,15 @@ function qualityCheck() {
     randY = int(random(arrayY.length));
   } // checks to see if options are equal
 
-  // **PROBLEM** Need to be able to know what pairs we've used
-  for (i = 0; i < used1.length; i++) {
-    if (randX * randY == used1[i]) {
+  // Need to be able to know what pairs we've used
+  for (i = 0; i < value.length; i++) {
+    if ((arrayX[randX] == value[i].option1 || arrayX[randX] == value[i].option2) && (arrayY[randY] == value[i].option1 || arrayY[randY] == value[i].option2)) {
       randY = int(random(arrayY.length));
     }
   } // checks to see if pair has been used
 }
 
-// assigns
+// assigns a new array with the winner
 function assignValue() {
   if (mouseX > width - 200 && mouseX < width - 50 && mouseY > 50 && mouseY < 150) {
     append(value, 
@@ -66,6 +130,7 @@ function assignValue() {
         option2:arrayY[randY],
         winner:arrayX[randX]
       });
+      dir = 0.4;
   } else if (mouseX > width - 200 && mouseX < width - 50 && mouseY > height - (height / 2) - 50 && mouseY < height - (height / 2) + 100) {
     append(value, 
       {
@@ -73,6 +138,7 @@ function assignValue() {
         option2:arrayY[randY],
         winner:"Z"
       });
+      dir = 0;
   } else if (mouseX > width - 200 && mouseX < width - 50 && mouseY > height - 150 && mouseY < height - 50) {
     append(value, 
       {
@@ -80,6 +146,7 @@ function assignValue() {
         option2:arrayY[randY],
         winner:arrayY[randY]
       });
+      dir = -0.4;
   } else {
     fill(0);
   }
