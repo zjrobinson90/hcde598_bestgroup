b95d04dcb8be70b4b02ad4fdea48e1879d1125c8
Josh <josh.leibsohn@gmail.com>
2017-03-11 15:31:28 -0800

    animations added

--- carEthics/sketch.js
+++ carEthics/sketch.js
@@ -2,8 +2,10 @@
 // Last edited 03/11/17
 // This is some code to crowd-source ethics for self-driving cars
 
-var choice1 = ["One kid", "Group of kids", "One adult", "Group of adults", "Paul Rudd", "Jeff Bezos", "Donald Trump", "The Pope"];
+var choice1 = [kid, adult, groupOfKids, groupOfAdults, rudd, musk, trump, pope];
+var label1 = ["One kid", "Group of kids", "One adult", "Group of adults", "Paul Rudd", "Jeff Bezos", "Donald Trump", "The Pope"];
 var choice2 = [];
+var label2 = [];
 var value = [];
 var results = [];
 var randX = 0;
@@ -20,6 +22,7 @@ function setup() {
   createCanvas(800, 600);
   for (i = 0; i < choice1.length; i++) {
     append(choice2, choice1[i]);
+    append(label2, label1[i]);
     append(results, "choice" + i);
     results[i] = 0;
   }
@@ -31,6 +34,7 @@ function setup() {
 function draw() {
   background(100, 200, 0);
   road();
+  details();
   car(carX, carY);
   showOptions();
   turnCarOn();
@@ -44,10 +48,6 @@ function draw() {
     text(results[3], 100, 100);
   }
   carReset();
-  
-  //DELETE THIS STUFF
-  text(value.length, 100, 100);
-  text(totalCombinations, 100, 150);
 }
 
 
@@ -111,9 +111,9 @@ function showOptions() {
   noStroke();
   textSize(15);
   fill(255);
-  text(choice1[randX], width - 100, 100);
+  choice1[randX](width - 120, 120);
   text("Kill yourself", width - 100, height - (height / 2));
-  text(choice2[randY], width - 100, height - 100);
+  choice2[randY](width - 120, height - 120);
 }
 
 
@@ -167,4 +167,88 @@ function assignValue() {
   } else {
     fill(0);
   }
+}
+
+function details () {
+  for (i = 0; i < 8; i++) {
+  fill(255);
+  rect(50 + i*100, height/2, 50, 20);
+  }
+ tree(200, 100);
+ tree(300, 100);
+ tree(250, 125);
+ tree(400, 520);
+ tree(350, 490);
+ tree(300, 520);
+}
+
+function tree (x, y) {
+  fill(89, 153, 6);
+  triangle(x, y, x + 50, y, x + 25, y - 60);
+  fill(153, 109, 9);
+  rect(x + 20, y, 10, 20);
+  }
+
+//top pos: width - 100, 100
+//bottom pos: width - 100, height - 100
+
+function adult (x, y) {
+  fill(200); //fills grey
+  ellipse(x, y, 30); //head
+  stroke(200); //stroke grey
+  strokeWeight(3); //stroke size 3
+  line(x, y + 15, x, y + 60); // body
+  line(x, y + 30, x + 10, y + 20); // right arm
+  line(x, y + 30, x - 12, y + 20); //left arm
+  line(x, y + 60, x + 10, y + 70); //right leg
+  line(x, y + 60, x - 12, y + 70); //left leg
+  fill(0); //fill black
+  ellipse(x - 5, y - 5, 7); //left eye
+  ellipse(x + 5, y - 5, 7); //right eye
+  ellipse(x, y + 8, 10) //mouth
+  noStroke(); //turn stroke off
+}
+
+function kid (x, y) {
+  fill(200); //fills grey
+  ellipse(x, y, 20); //head
+  stroke(200); //stroke grey
+  strokeWeight(3); //stroke 3
+  line(x, y + 10, x, y + 35); //body
+  line(x, y + 25, x + 8, y + 20); //right arm
+  line(x, y + 25, x - 8, y + 20); //left arm
+  line(x, y + 35, x + 8, y + 45); //right leg
+  line(x, y + 35, x - 8, y + 45); //left leg
+  fill(0); //fill black
+  ellipse(x - 4, y - 3, 6); //left eye
+  ellipse(x + 4, y - 3, 6); //right eye
+  ellipse(x, y + 4, 7) //mouth
+  noStroke(); //turn stroke off 
+}
+
+function groupOfKids (x, y) {
+  kid(x, y);
+  kid(x + 30, y + 30);
+  kid(x - 30, y + 30);
+}
+function groupOfAdults (x, y) {
+  adult(x, y);
+  adult(x + 30, y + 30);
+  adult(x + 30, y + 30);
+}
+
+function pope (x, y) {
+  adult(x, y);
+}
+
+function trump (x, y) {
+  adult(x, y);
+}
+
+function rudd (x, y) {
+  adult(x, y);
+}
+
+function musk (x, y) {
+  adult(x, y);
 }
